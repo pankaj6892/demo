b3cf55d14e36480087289295c8ac822195e2c17a
pankaj singh <pankaj.singh@ics-global.in>
2023-03-27 17:25:39 +0530

    change in app js message

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test
+          Intech Testing
         </p>
         <a
           className="App-link"
